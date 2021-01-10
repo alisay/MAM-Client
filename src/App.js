065bd1919b531cea30bd1b637cce66461aa7b807
Alisa <alisa.blakeney@gmail.com>
2021-01-11 08:30:32 +1100

    add google map container

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import {BrowserRouter} from "react-router-dom";
 import 'fontsource-roboto';
 
-import NavBar from "./components/navbar";
+import NavBar from "./components/Navbar.js";
+import MapContainer from "./components/Map.js";
 
 
 function App() {
   return (<BrowserRouter>
   <NavBar />
   <main>
-
+    <MapContainer />
   </main>
   </BrowserRouter>
   );
