6a57f02cfa26fa594bdffc1e8f5fd4c695c892f4
Alisa <alisa.blakeney@gmail.com>
2021-01-10 16:44:02 +1100

    create map outline

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import {BrowserRouter} from "react-router-dom";
 import 'fontsource-roboto';
 
-import NavBar from "./components/navbar";
+import NavBar from "./components/Navbar.js";
+import Map from "./components/Map.js";
 
 
 function App() {
   return (<BrowserRouter>
   <NavBar />
   <main>
-
+    <Map />
   </main>
   </BrowserRouter>
   );
---
+++ src/components/Map.js
@@ -0,0 +1,16 @@
+import React from "react";
+import mapboxgl from "mapbox-gl"
+
+const Map = () => {
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWxpc2F5IiwiYSI6ImNraTN3bTB5NDBhMmczNG50dW5wZHV3YTQifQ.pPW2nXUOGOuAVmtTDZh7mw';
+
+
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default Map;
