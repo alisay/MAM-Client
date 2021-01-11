b65fdfb46232e0305d7f89a7c89b7fddb1c7dcbd
Alisa <alisa.blakeney@gmail.com>
2021-01-11 12:27:50 +1100

    style infowindows

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import { GoogleMap, LoadScript, Marker, InfoWindow } from "@react-google-maps/api";
-import axios from "axios"
+import axios from "axios";
+import About from "./About.js"
 
 
 const MapContainer = () => {
@@ -27,13 +28,13 @@ const MapContainer = () => {
     })
 
     // on mount
-    // useEffect(() => {
-    //     axios.get("data.json")//update to be actual endpoint once backend api working
-    //         .then((res) => {
-    //             setArtworks(res.data)
-    //         })
-    //         .catch(console.log)
-    //     }, [])
+    useEffect(() => {
+        axios.get("data.json")//update to be actual endpoint once backend api working
+            .then((res) => {
+                setArtworks(res.data)
+            })
+            .catch(console.log)
+        }, [])
 
     const mapStyles = {        
         height: "100vh",
@@ -42,44 +43,6 @@ const MapContainer = () => {
     const defaultCenter = {
         lat: -37.813600, lng: 144.963100
     }
-
-  const fartworks = [
-    {
-      name: "Port Phillip Monument",
-      geom: { 
-        lat: -37.8056957854241,
-        lng: 144.907291041632 
-      },
-    },
-    {
-      name: "Blowhole",
-      geom: { 
-        lat: -37.8220182164578,
-        lng: 144.946871022845
-      },
-    },
-    {
-      name: "Federation Bells",
-      geom: { 
-        lat: -37.8186738864394,
-        lng: 144.974167165492
-      },
-    },
-    {
-      name: "Location 4",
-      geom: { 
-        lat: 41.3797,
-        lng: 2.1682
-      },
-    },
-    {
-      name: "Location 5",
-      geom: { 
-        lat: 41.4055,
-        lng: 2.1915
-      },
-    }
-  ];
   
   return (
      <LoadScript
@@ -97,28 +60,27 @@ const MapContainer = () => {
           }
         {/* artwork markers */}
           {
-             fartworks.map(artwork => {
-               return (
-               <Marker key={artwork.name} position={artwork.geom} onClick={() => onSelect(artwork)}
-               />
-               )
-             })
-
-            // use this with json
-            // artworks && artworks.map(artwork => 
-            //     <Marker key={artwork.name} position={{lat: parseFloat(artwork.geom.lat), lng: parseFloat(artwork.geom.lon)}}/>
-            // )
+            artworks && artworks.map(artwork => 
+                <Marker key={artwork._id} position={{lat: parseFloat(artwork.geom.latitude), lng: parseFloat(artwork.geom.longitude)}} onClick={() => onSelect(artwork)}/>
+            )
           }
           {/* infoWindows */}
           {
             selected.geom && 
             (
             <InfoWindow
-            position={selected.geom}
+            position={{lat: parseFloat(selected.geom.latitude), lng: parseFloat(selected.geom.longitude)}}
             clickable={true}
             onCloseClick={() => setSelected({})}
             >
-            <p>{selected.name}</p>
+            <div>
+                {/* <img src={selected.image} /> */}
+                <p>{selected.name}</p>
+                <p>{selected.addresspt}</p>
+                <p>ARTIST: {selected.artist}</p>
+                <p>{selected.artdate}</p>
+                <p>{selected.structure}</p>
+            </div>
             </InfoWindow>
             )
           }
