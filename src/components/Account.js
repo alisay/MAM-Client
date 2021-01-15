d12421e772b3b6e21091ffb50637f1796f4dda83
Alisa <alisa.blakeney@gmail.com>
2021-01-15 13:45:44 +1100

    updated form fields to match database but still need to correct fetch so that it authenticates properly

--- src/components/Account.js
+++ src/components/Account.js
@@ -2,7 +2,6 @@ import React, { useState, useContext } from 'react';
 import { TextField, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Geocode from 'react-geocode';
-import axios from 'axios';
 import Login from "./Login.js"
 import {TokenContext} from "./TokenContext.js"
 
@@ -26,6 +25,7 @@ const useStyles = makeStyles((theme) => ({
 export default function Account() {
     const classes = useStyles();
     const [artwork, setArtwork] = useState({});
+    const [blob, setBlob] = useState(null);
     const context = useContext(TokenContext)
   
     if(!context.token) {
@@ -34,11 +34,14 @@ export default function Account() {
 
     const handleAddressGet = (event) => {
         event.preventDefault();
-        Geocode.fromAddress(artwork.addresspt).then(
+        console.log(artwork.location)
+        Geocode.fromAddress(artwork.location).then(
             response => {
               const { lat, lng } = response.results[0].geometry.location;
               const location = {"geom":{"latitude":lat, "longitude":lng}}
               setArtwork(location)
+              console.log(location)
+              console.log(artwork)
             },
             error => {
               console.error(error);
@@ -49,23 +52,32 @@ export default function Account() {
   
     const handleFormSubmit = (event) => {
         event.preventDefault();
-          
-        console.log(JSON.stringify(artwork))
-        axios.post("https://melbourneartmap.herokuapp.com/artworks/new",
-        {
-            body: JSON.stringify(artwork)
-        },    
-        {
-                auth:{
-                    username: "admin",
-                    password: "123456"
-                }
-            }
-         ) .then(response=>{
-             console.log(response)
-            //  return response.json()
-            })
-          .catch(console.log)
+
+        var myHeaders = new Headers();
+        myHeaders.append("Cookie", "connect.sid=s%3AYIuwpNjTlbv1oUs2bTeyyQfzSlVPPkQA.9y1Gr1owmqFmWnZWT%2FLEHiEfr9UWZLm9aTSu7im04sk");
+
+        var formdata = new FormData();
+        formdata.append("title", artwork.title);
+        formdata.append("location", artwork.location);
+        formdata.append("artist", artwork.artist);
+        formdata.append("date", artwork.date);
+        formdata.append("details", artwork.details);
+        formdata.append("latitude", artwork.geom.latitude);
+        formdata.append("longitude", artwork.geom.longitude);
+        formdata.append("image", blob);
+
+        var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: formdata,
+        redirect: 'follow'
+        };
+        
+        fetch("https://melbourneartmap.herokuapp.com/artworks/new", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+        
     }
 
     return (
@@ -73,8 +85,8 @@ export default function Account() {
         <h1>Add Artwork</h1>
       <form className={classes.root} noValidate autoComplete="off">
         <div>
-          <p><TextField required id="standard-required" label="Required" defaultValue="Name of artwork" onChange={event=>setArtwork({...artwork, "name": event.target.value})}/></p>
-          <p><TextField required id="standard-required" label="Required" defaultValue="Address" onChange={event=>setArtwork({...artwork, "addresspt": event.target.value})}/></p>
+          <p><TextField required id="standard-required" label="Required" defaultValue="Name of artwork" onChange={event=>setArtwork({...artwork, "title": event.target.value})}/></p>
+          <p><TextField required id="standard-required" label="Required" defaultValue="Address" onChange={event=>setArtwork({...artwork, "location": event.target.value})}/></p>
           <Button type="submit" variant="contained" onClick={handleAddressGet}>Get address</Button>
           <p><TextField
           id="standard-required"
@@ -82,10 +94,21 @@ export default function Account() {
           value={artwork && artwork.geom ? `${artwork.geom.latitude}, ${artwork.geom.longitude}` : ""}
         /></p>
           <p><TextField required id="standard-required" label="Required" defaultValue="Artist" onChange={event=>setArtwork({...artwork, "artist": event.target.value})}/></p>
-          <p><TextField required id="standard-required" label="Required" defaultValue="Year of construction" onChange={event=>setArtwork({...artwork, "artdate": event.target.value})}/></p>
-          <p><TextField required id="standard-required" label="Required" defaultValue="Material" onChange={event=>setArtwork({...artwork, "structure_": event.target.value})}/></p>
+          <p><TextField required id="standard-required" label="Required" defaultValue="Year of construction" onChange={event=>setArtwork({...artwork, "date": event.target.value})}/></p>
+          <p><TextField required id="standard-required" label="Required" defaultValue="Material" onChange={event=>setArtwork({...artwork, "details": event.target.value})}/></p>
+          <Button
+            variant="contained"
+            component="label"
+          >
+            Upload Image
+            <input
+                type="file"
+                onChange={(e) => setBlob(e.target.files[0])}
+            />
+          </Button>
         </div>
         <div className={classes.submit}>
+          {/* disable button when loading */}
         <Button type="submit" variant="contained" color="secondary" onClick={handleFormSubmit}>Submit</Button>
         </div>
       </form>
