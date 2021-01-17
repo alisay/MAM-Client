76b38fe13689d415e82377c28f7c3a638b0e3e1a
Alisa <alisa.blakeney@gmail.com>
2021-01-17 15:26:56 +1100

    style dashboard and implement delete artwork functionality

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import 'fontsource-roboto';
 
 import NavBar from "./components/navbar.js";
 import MapContainer from "./components/Map.js";
-import Account from "./components/Account.js";
-
+import Dashboard from "./components/Dashboard.js";
 
 function App() {
   const [ selected, setSelected ] = useState({});
@@ -20,7 +19,7 @@ function App() {
         <MapContainer selected={selected} setSelected={setSelected} />
       </Route>
       <Route exact path="/account">
-        <Account />
+        <Dashboard />
       </Route>
     </Switch>
   </main>
---
+++ src/components/Dashboard.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState, useContext } from "react";
+import Account from "./Account.js";
+import {TokenContext} from "./TokenContext.js";
+import axios from "axios"
+import { DataGrid } from '@material-ui/data-grid';
+import { Grid, Button, Container } from '@material-ui/core';
+
+
+export default function Dashboard () {
+    const context = useContext(TokenContext)
+    const [ artworks, setArtworks ] = useState(undefined)
+    const [ selected, setSelected ] = useState("")
+
+    useEffect(() => {
+        axios.get("https://melbourneartmap.herokuapp.com/artworks/")
+            .then((res) => {
+                setArtworks(res.data)
+            })
+            .catch(console.log)
+        }, [])
+
+        const columns = [
+            { field: 'id', headerName: 'ID', width: 300 },
+            { field: 'artworkName', headerName: 'Artwork Title', width: 300 },
+            { field: 'artist', headerName: 'Artist', width: 300 },
+          ];
+          
+        const rows = artworks ? artworks.map(artwork=>{
+            return { id: artwork._id, artworkName: artwork.name, artist: artwork.artist }
+        }) : []
+          
+
+        const handleAddArtwork = () => {
+
+        }
+
+        const handleDeleteArtwork = () => {
+            var myHeaders = new Headers();
+            // myHeaders.append("Cookie", "connect.sid=s%3AbvU1pAQSo4S9hUHkM8vnMk-YKiIXLg3N.U52w521hIr7RE2PcPv41UHXi%2BPQY%2Bfb6u8TmRbeVL3w");
+
+            var requestOptions = {
+            method: 'DELETE',
+            headers: myHeaders,
+            redirect: 'follow'
+            };
+
+            fetch(`https://melbourneartmap.herokuapp.com/artworks/${selected}`, requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));            
+        }
+
+        const handleSelectionChange = (event) =>{
+            setSelected(event.rowIds[0])
+        }
+
+    return(
+        <>
+        <Container>
+        <h2>Welcome {context.token.admin.username}! </h2>
+            {/* <div>{artworks && artworks.map(artwork=>artwork.name)}</div> */}
+            <div style={{ height: 400, width: '100%' }}>
+                <DataGrid rows={rows} columns={columns} pageSize={5} onSelectionChange={handleSelectionChange} />
+            </div>
+            <p></p>
+            <div>
+            <Grid container spacing={3}>
+                <Grid container item xs>
+                    <Button variant="contained" color="secondary" onClick={handleAddArtwork}>Add New Artwork</Button>
+                </Grid>
+                <Grid container item xs>
+                    <Button variant="contained" color="secondary" onClick={handleAddArtwork}>Edit Selected Artwork</Button>
+                </Grid>
+                <Grid container item xs={3} >
+                    <Button variant="contained" color="secondary" onClick={handleDeleteArtwork}>Delete Selected Artwork</Button>
+                </Grid>
+            </Grid>
+            </div>
+            {/* <Account /> */}
+        </Container>
+        </>
+    )
+}
